6106769411fa1bc8df2c760703ce0c71160ed6fc
Anton Cwik <anton@cwik.biz>
2018-02-25 22:05:18 -0800

    Partly working

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -7,4 +7,25 @@
 var getElementsByClassName = function(className
 ) {
   // your code here
+  var results = [];
+   function getClass(nodeList){
+     var childList = nodeList.children;
+     childList.forEach(function(node) {
+     //1st level body check
+      // **Here, only include the element if Array#every is true,
+      // **where you give Array#every a function that does your
+      // classList.contains on the name for that iteration
+      if(node.classList  && node.classList.contains(className)){
+        results.push(node);
+      }
+
+      //has children, recurse
+      if(node.children) {
+        results = results.concat(getClass(node));
+      }
+    });
+   }
+   getClass(document.body);
+   return results;
+  
 };
